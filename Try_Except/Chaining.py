82ee662b27bc2993f556fd42ece54398fc78c2f5
Cherry <charan172003@gmail.com>
2025-12-17 11:33:04 +0530

    Completed Error Handelings

---
+++ Try_Except/Chaining.py
@@ -0,0 +1,28 @@
+
+# Error Chaining
+# try:
+#     a = int(input("Enter a number: ")) # TypeError may occur
+#     b = int(input("Enter another number: "))
+#     try:
+#         c = a/b # ZeroDivision Error may occur
+#     except Exception as e:
+#         print(e)
+# except Exception as e:
+#     print(e)
+# # else:
+# #     print(c)
+
+# try:
+#     x = int(input("Enter a number: "))
+#     assert x>0, "Number Should be Positive"
+#     print(x)
+# except Exception as e:
+#     print(e)
+
+# try:
+#     a = input("Enter Something: ")
+# except EOFError:
+#     print("EOFError")
+
+
+StopIteration
