59409d36df211cc6c478a99fd4f685052888aee0
Cherry <charan172003@gmail.com>
2025-12-17 11:48:20 +0530

    Added two lines

--- encypt.py
+++ encypt.py
@@ -1,4 +1,6 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
+alphabet = list(map(chr,range(97,123))) # This can be used
+Capital_alphabet = list(map(chr,range(65,91)))
 
 def encrypt(ori_text,shift):
     cipher_text = ""
